c5e8265a50cb83dced3fad29c379c9124a7bdbf4
daniel <daniel@methodandclass.com>
2020-04-14 11:23:19 +0100

    Refactor file system to include docker.

---
+++ src/app/resources/vuejs/src/Options.js
@@ -0,0 +1,30 @@
+export let Options = {
+    'minEmpires': 6,
+    'maxEmpires': 11,
+    'maxSolarSystems': 1000,
+    'minBodies' : 1,
+    'maxBodies' : 5,
+    'maxOrbits' : 20,
+
+    'fleetCost': 100000,
+    'maxFleets': 50,
+    'maxXP': 1000,
+    'maxHP': 1000,
+    'maxRank': 3,
+
+    'border' : 0,
+    'distanceScale': 10000,
+    'interval': 1,
+    'defaultZoom': 1,
+    'maxZoom': 10000,
+
+    'startCredits' : 10000000,
+
+    'color': ['#00D2BE', '#DC0000', '#1E41FF', '#FF8700', '#FFF500', '#F596C8', '#469BFF', '#9B0000', '#F0D787', '#FFFFFF', '#004225'],
+    'colorName': ['Mercedes', 'Ferrari', 'Red Bull', 'McLaren', 'Renault', 'Racing Point', 'Toro Rosso', 'Alfa Romeo', 'Haas', 'Williams', 'Lotus'],
+
+    'render': {
+       'fleetLines': true,
+       'fleets': true,
+    } ,
+};
